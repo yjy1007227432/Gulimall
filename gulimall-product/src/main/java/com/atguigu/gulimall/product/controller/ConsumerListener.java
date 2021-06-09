9c25afc21c53cbcada4df9785fe3150f4e35d260
yaojunyi <1007227432@qq.com>
2021-06-09 23:13:05 +0800

    kafka

---
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/controller/ConsumerListener.java
@@ -0,0 +1,15 @@
+package com.atguigu.gulimall.product.controller;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerListener {
+
+    @KafkaListener(topics = "testTopic")
+    public void onMessage(String message){
+        //insertIntoDb(buffer);//这里为插入数据库代码
+        System.out.println(message);
+    }
+
+}
